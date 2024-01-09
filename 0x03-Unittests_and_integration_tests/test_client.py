37bee105ef1a15caeb37304698c8606f25ba952e
phurhard <phurhardeen@gmail.com>
2024-01-09 01:38:24 +0100

    clientsGithub

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,6 +4,7 @@ import unittest
 from unittest.mock import patch
 from client import GithubOrgClient
 from parameterized import parameterized
+from typing import Dict
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -13,14 +14,14 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc"),
     ])
     @patch('client.get_json')
-    def test_org(self, orgs, mock_request):
+    def test_org(self, orgs: str, mock_request: patch):
         """Test that the method org returns the required result"""
 
+        url = f"https://api.github.com/orgs/{orgs}"
         mock_request.return_value = {"key": "value"}
 
         instance = GithubOrgClient(orgs)
 
-        result = instance.org
+        result: Dict = instance.org
         self.assertEqual(result, {"key": "value"})
-        mock_request.assert_called_once_with(f"\
-            https://api.github.com/orgs/{orgs}")
+        mock_request.assert_called_once_with(url)
